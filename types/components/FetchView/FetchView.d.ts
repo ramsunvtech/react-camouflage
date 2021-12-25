4ed3d1bbab5aaaaea54fc03cd7dccf8e98927a1a
Venkat.R <ramsunvtech@gmail.com>
2021-12-25 19:30:48 +0800

    tech(types): add type declaration

--- types/components/FetchView/FetchView.d.ts
+++ types/components/FetchView/FetchView.d.ts
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 export default FetchView;
 
 interface FetchViewProps<T = {}> {
     status: string;
+    statusMapping: any;
     children: React.Element;
 }
 
 declare function FetchView(props: FetchViewProps): any;
+
 declare namespace FetchView {
     export { DefaultView as Initial };
     export { FetchingView as Fetching };
     export { FetchedView as Fetched };
     export { ErrorView as Error };
     export namespace propTypes {
+        const statusMapping: any;
         const status: any;
         const children: any;
     }
@@ -21,7 +24,8 @@ declare namespace FetchView {
 declare function DefaultView({ children }: {
     children: any;
 }): any;
-declare function FetchingView({ children }: {
+declare function FetchingView({ loadingText, children }: {
+    loadingText?: string;
     children: any;
 }): any;
 declare function FetchedView({ children }: {
